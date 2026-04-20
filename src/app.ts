614452e3d9b014f4235600a0dbbb5d4a0fc4d68d
mahmoudsabry12 <mahmoudsabry.udacity@gmail.com>
2026-04-20 17:29:36 +0200

    integrate queue monitoring dashboard using Bull Board

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import  emailRoute from './api/email.route'
 export const app = express()
+import { bullBoardRouter } from './config/bullBoard'
 
 app.use(express.json())
+app.use('/admin/queues', bullBoardRouter)
 app.use('/api',  emailRoute)
 app.use('/', async (req, res) => {
     res.json({ message: 'Welcome to the Scalable Job Queue API' })
